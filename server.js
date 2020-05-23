d870f7493555236c2c91c92c081e9b27a9141d03
ravyrafael <ravyrafael@gmail.com>
2020-05-23 15:29:16 -0300

    cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-
 const app = express();
-const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/api/mensagem', (req, res) => {
   res.send({ express: 'Hello From Express' });
