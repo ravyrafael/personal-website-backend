5ef02799812f83886c70914ec666d309054b9589
ravyrafael <ravyrafael@gmail.com>
2020-05-23 16:13:03 -0300

    server prototype finished

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/mensagem', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.post('/api/mensagem', (req, res) => {
+    var date = new Date().toLocaleDateString()
+    const logger = fs.createWriteStream("messages/"+req.body.name+date+ ".json", {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+      })
+      var values = req.body;
+      values.date = date
+      logger.write(JSON.stringify(req.body) +"\n");
+      res.send(req.body);
 });
 
 app.get('/', (req, res) => {
     res.send({ express: 'ok!' });
   });
+
+app.get('/files', (req, res) => {
+    var files = fs.readdirSync("messages")
+    res.send({files});
+  });
+app.post('/download', (req, res) => {
+    console.log(req.query)
+    var file = fs.readFileSync(`messages/${req.query.name}`);
+    res.send({file}); // Set disposition and send it.
+  });
+
 
   app.listen(process.env.PORT || 8000); 
